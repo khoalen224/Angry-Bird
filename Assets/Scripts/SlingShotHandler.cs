0f13ef495442c2ffcaa0d7130dbdd13d13a9c3a4
khoalen224 <khoalen205@gmail.com>
2025-06-04 21:22:21 +0700

    Initial commit

---
+++ Assets/Scripts/SlingShotHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; // Ensure you have the Input System package installed
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            // will run
+            DrawSlingShot();
+        }
+    }
+    private void DrawSlingShot()
+    {
+
+    }
+}
