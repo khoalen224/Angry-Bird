dfbf432f334a9f65ffb61c8ec1b6cf29b1db7d81
vitt91204 <somethingthaticantforget@gmail.com>
2025-06-07 18:12:27 +0700

    Made the Slingshot Strips

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -6,17 +6,32 @@ using UnityEngine.InputSystem; // Ensure you have the Input System package insta
 
 public class SlingShotHandler : MonoBehaviour
 {
+    [SerializeField] private LineRenderer leftLineRenderer;
+    [SerializeField]private LineRenderer rightLineRenderer;
+
+    [SerializeField]private Transform leftStartPosition;
+    [SerializeField] private Transform rightStartPosition;
     // Update is called once per frame
     private void Update()
     {
-        if(Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.isPressed)
         {
             // will run
             DrawSlingShot();
+
         }
     }
     private void DrawSlingShot()
     {
+        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        SetLine(touchPosition);
+    }
+    private void SetLine(Vector2 position)
+    {
+        leftLineRenderer.SetPosition(0, position);
+        leftLineRenderer.SetPosition(1, leftStartPosition.position);
 
+        rightLineRenderer.SetPosition(0, position);
+        rightLineRenderer.SetPosition(1, rightStartPosition.position);
     }
 }
