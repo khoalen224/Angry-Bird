182e98849437471cb4f358ce131985c88337c436
vitt91204 <somethingthaticantforget@gmail.com>
2025-06-18 11:03:42 +0700

    barely

--- Assets/Scripts/AngryBird.cs
+++ Assets/Scripts/AngryBird.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class AngryBird : MonoBehaviour
 {
-      private Rigidbody2D _rb;
-      private CircleCollider2D _circleCollider;
+      private Rigidbody2D rigidBody;
+      private CircleCollider2D circleCollider;
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _circleCollider = GetComponent<CircleCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
     public void Start()
     {
-        _rb.bodyType = RigidbodyType2D.Kinematic;
-        _circleCollider.enabled = false;
+        rigidBody.bodyType = RigidbodyType2D.Kinematic;
+        circleCollider.enabled = false;
     }
     public void LaunchBird(Vector2 direction, float force)
     {
-        _rb.bodyType = RigidbodyType2D.Dynamic;
-        _circleCollider.enabled = true;
+        rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        circleCollider.enabled = true;
 
         //apply force
-        _rb.AddForce(direction * force, ForceMode2D.Impulse);
+        rigidBody.AddForce(direction * force, ForceMode2D.Impulse);
     }
 }
